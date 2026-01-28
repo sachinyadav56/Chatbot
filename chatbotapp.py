187f1881e6f556aa94fd585d4959f7755cf91851
sachinyadav56 <haramanyadav357@gmail.com>
2026-01-28 11:08:52 +0530

    Add new line of code Covert response to user language

--- chatbotapp.py
+++ chatbotapp.py
@@ -3,15 +3,11 @@ import pickle, json, random
 from deep_translator import GoogleTranslator
 from langdetect import detect, DetectorFactory
 
-
-
-
 app = Flask(__name__)
 
 model = pickle.load(open("model.pkl", "rb"))
 vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 intents = json.load(open("intents.json"))
-
 @app.route("/chatbot", methods=["GET", "POST"])
 def chat():
     if request.method == "GET":
@@ -20,15 +16,24 @@ def chat():
     data = request.get_json()
     user_input = data.get("message", "")
 
+    # Detect input language
+    user_lang = detect(user_input)
+
+    # Convert  input → English
     translated_input = GoogleTranslator(source='auto', target='en').translate(user_input)
 
+      # Machin Lag processing
     X = vectorizer.transform([translated_input])
     tag = model.predict(X)[0]
 
     for intent in intents["intents"]:
         if intent["tag"] == tag:
-            reply = random.choice(intent["responses"])
-            return jsonify({"reply": reply})
+            reply_en = random.choice(intent["responses"])
+
+                 # Convert English reply → user language
+            final_reply = GoogleTranslator(source='en', target=user_lang).translate(reply_en)
+
+            return jsonify({"reply": final_reply})
 
     return jsonify({"reply": "I don't understand"})
 
@@ -38,3 +43,5 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
